d5b56d7aed1ef4698d711dd9b9116e2db3eed50e
Michael Cooper <mic159@gmail.com>
2014-02-15 20:14:57 +1100

    Width and height constants

--- PongGame.cpp
+++ PongGame.cpp
@@ -1,21 +1,20 @@
-
-#define WHITE 1
-
-#include <arduino.h>
+#include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include "Paddle.h"
 #include "Ball.h"
 #include "PongGame.h"
 
-
+#define WHITE 1
+#define WIDTH 128
+#define HEIGHT 64
 
 PongGame::PongGame()
-: lpaddle(0, 0), rpaddle(128 - 2, 64 - 14)
+: lpaddle(0, 0), rpaddle(WIDTH - 2, HEIGHT - 14)
 , ball(2, 2, 1, 1)
 , lscore(0), rscore(0)
 , pause(0)
 {
-  lpaddle.y = random(0, 64 - lpaddle.h);
+  lpaddle.y = random(0, HEIGHT - lpaddle.h);
   ball.x = lpaddle.w;
   ball.y = lpaddle.y + (lpaddle.h / 2);
   ball.dx = 1;
@@ -41,13 +40,13 @@ void PongGame::update(uint16_t hour, uint16_t minute) {
 
   if (hit) {
     // Wohoo, a score!
-    if (ball.x > 128 / 2) {
+    if (ball.x > WIDTH / 2) {
       lscore += 1;
     } else {
       rscore += 1;
     }
     // New game (ball on lpaddle)
-    lpaddle.y = random(0, 64 - lpaddle.h);
+    lpaddle.y = random(0, HEIGHT - lpaddle.h);
     ball.x = lpaddle.w;
     ball.y = lpaddle.y + (lpaddle.h / 2);
     ball.dx = 1;
@@ -60,8 +59,8 @@ void PongGame::draw(Adafruit_GFX& display) const {
   lpaddle.draw(display);
   rpaddle.draw(display);
   // Middle line
-  for(uint8_t i = 0; i < 64 / 4; i = i + 2) {
-    display.fillRect(128 / 2 - 1, i * 4, 2, 4, WHITE);
+  for(uint8_t i = 0; i < HEIGHT / 4; i = i + 2) {
+    display.fillRect(WIDTH / 2 - 1, i * 4, 2, 4, WHITE);
   }
   drawScore(display);
   ball.draw(display);
@@ -72,10 +71,10 @@ void PongGame::drawScore(Adafruit_GFX& display) const {
   static const int font_w = 10;
   static const int font_sp = 4;
 
-  if (lscore > 9) lscore_x = 128 / 2 - font_w - font_w - font_sp - 10;
-  else lscore_x = 128 / 2 - font_w - 10;
+  if (lscore > 9) lscore_x = WIDTH / 2 - font_w - font_w - font_sp - 10;
+  else lscore_x = WIDTH / 2 - font_w - 10;
 
-  rscore_x = 128 / 2 + 10;
+  rscore_x = WIDTH / 2 + 10;
 
   display.setTextColor(WHITE);
 
